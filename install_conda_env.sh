f018a32bf3ba10aa3b796cd2a1bdbd18769532c8
asntech <azez.khan@gmail.com>
2022-09-19 18:12:26 +0200

    added option to install local copy of TitanCNA and also Mutect

--- install_conda_env.sh
+++ install_conda_env.sh
@@ -8,19 +8,11 @@ echo "$(date): Installing ith.Variant conda env..."
 conda create -n ith.variant --file ${CODE_DIR}/requirements.txt \
   --override-channels -c bioconda -c conda-forge -c defaults -c compbiocore -y
 
+conda activate ith.variant
+
+R -e 'install.packages("pkgs/TitanCNA_1.26.0.tar.gz")'
+
+conda install -c compbiocore mutect=1.1.6
+
 echo "$(date): Installed ith.Variant Successfully!!"
-
-cd pkgs
-zlib_version=1.2.12
-wget http://www.zlib.net/zlib-${zlib_version}.tar.gz
-tar -xvzf zlib-${zlib_version}.tar.gz
-
-cd zlib-${zlib_version}
-
-./configure --prefix=./zlib
-
-make install
-
-#wget https://github.com/pezmaster31/bamtools/archive/refs/tags/v2.5.2.tar.gz
-
 
